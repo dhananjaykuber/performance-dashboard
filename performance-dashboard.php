f7a0cc24296c5b553f39cb25d9f019a4853621da
dhananjaykuber <dnkuber2002@gmail.com>
2024-07-04 14:50:24 +0530

    feat: create main plugin file and add constants

---
+++ performance-dashboard.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Performance Dashboard
+ * Plugin URI: https://github.com/dhananjaykuber/performance-dashboard
+ * Description: Collect and display Core Web Vitals data in WordPress dashboard.
+ * Version: 1.0.0
+ * Author: Dhananjay Kuber
+ * Author URI: https://github.com/dhananjaykuber/
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: performance-dashboard
+ *
+ * @package PerformanceDashboard
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+// Define plugin constants.
+define( 'PERFORMANCE_DASHBOARD_VERSION', '1.0.0' );
+define( 'PERFORMANCE_DASHBOARD_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'PERFORMANCE_DASHBOARD_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
